0eac6efb7c36d28608a4db73f9181d60207837e3
fere122 <06blake.krueger@gmail.com>
2023-10-31 08:19:34 -0400

    something

--- ProjectOrangatang.py
+++ ProjectOrangatang.py
@@ -11,7 +11,15 @@ def gasLevelGauge():
     currentGasLevel = random.choice(gasLevelList)
     return currentGasLevel
 
+# Function with a list of GasStations
+def listOfGasStations():
+    gasStations = ["Shell","Speedway","Exxon","Meijer","Costco","Marathon","BP","Circle K","Wesco"]
+    gasStationnearby = random.choice(gasStations)
+    return gasStationnearby
+
 #Funtion will call the gaLevelGauge to determine gal level and find a close gas station if low
 def gasLevelAlert():
     milesToGasStationLow = round(random.uniform(1,25),1)
     milesToGasStationquarterTank = round(random.uniform(25.1, 50), 1)
+
+
